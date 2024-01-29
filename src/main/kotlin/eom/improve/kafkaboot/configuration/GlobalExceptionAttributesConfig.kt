0ab49f3ccb9be3b81a59caa6838882fa22a662bd
stephano-tri <mtriumph23@gmail.com>
2024-01-29 18:00:52 +0900

    FEATURE: global error exception config added #1

---
+++ src/main/kotlin/eom/improve/kafkaboot/configuration/GlobalExceptionAttributesConfig.kt
@@ -0,0 +1,7 @@
+package eom.improve.kafkaboot.configuration
+
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes
+
+class GlobalExceptionAttributesConfig : DefaultErrorAttributes {
+
+}
